a5bdf8d4e811fbb8ab12ad8d74feef363dcb95e1
Ivansallas <kiss_ivan@hotmail.com>
2025-06-23 22:41:31 -0300

    up

---
+++ main.js
@@ -0,0 +1,22 @@
+const numeroSenha = document.querySelector(".parametros-senha__texto");
+const botaoSenha = document.querySelectorAll(".parametros-senha__botao");
+
+let tamanhoSenha = 0;
+numeroSenha.textContent = tamanhoSenha;
+
+console.log(botaoSenha);
+
+botaoSenha[0].onclick = diminuiTamanhoSenha = () => {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+};
+
+botaoSenha[1].onclick = aumentaTamanhoSenha = () => {
+    if (tamanhoSenha < 20) {
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+};
+
